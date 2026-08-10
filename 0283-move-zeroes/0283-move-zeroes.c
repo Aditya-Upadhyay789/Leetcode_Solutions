7f84b91155f11f4e625dfc39a932f2a479bb693f
Aditya-Upadhyay789 <adityaupadhyay0906@gmail.com>
2026-08-11 00:32:08 +0530

    Time: 0 ms (100%), Space: 20.2 MB (84.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -0,0 +1,20 @@
+void moveZeroes(int* nums, int numsSize) {
+    int k=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=0){
+            nums[k]=nums[i];
+            k++;
+        }
+       
+    }
+     
+    for(int j=k; j<=numsSize-1; j++){
+        nums[j]=0;
+        
+        
+    } 
+    k--;
+     
+    
+    
+}
