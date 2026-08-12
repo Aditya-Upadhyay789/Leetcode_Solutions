c616c834c932a2cec3baad4513d090a671b9fe08
Aditya-Upadhyay789 <adityaupadhyay0906@gmail.com>
2026-08-12 12:28:31 +0530

    Time: 1 ms (57.27%), Space: 12.7 MB (12.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -1,30 +1,28 @@
 int lengthOfLongestSubstring(char* s) {
-    int hash[256];
-    int n = strlen(s);
-    int l=0;
-    int r=0;
-    int maxlen=0;
-    int len=0;
+int hash[256];
+int l=0;
+int r=0;
+int len=0;
+int maxlen=0;
+int n=strlen(s);
 
-    for(int i=0;i<256;i++){
-        hash[i]=-1;
-    }
+for(int i=0;i<256;i++){
+    hash[i]=-1;
+}
 
-    while(r<n){
-        if(hash[s[r]]!=-1){
-            if(hash[s[r]]>=l){
-                l=hash[s[r]]+1;
-
-
-            }
+while(r<n){
+    if(hash[s[r]]!=-1){
+        if(hash[s[r]]>=l){
+            l=hash[s[r]]+1;
         }
-
-        len = r-l+1;
-        maxlen= (maxlen > len)? maxlen : len;
-        hash[s[r]]=r;
-        r++;
-
     }
+    len=r-l+1;
+    if(len>maxlen){
+        maxlen=len;
+    }
+    hash[s[r]]=r;
+    r++;
+}
 return maxlen;
 }
 
